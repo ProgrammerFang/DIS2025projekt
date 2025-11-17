6bdabdf0ff31a10ee02e79d7b3b90a02ef03e753
ProgrammerFang <alfa24ae@student.cbs.dk>
2025-11-17 21:22:26 +0100

    port nummer

--- disprojekt2025/app.js
+++ disprojekt2025/app.js
@@ -110,4 +110,9 @@ app.use(function(err, req, res, next) {
   `);
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server kører på http://0.0.0.0:${PORT}`);
+});
+
 module.exports = app;
