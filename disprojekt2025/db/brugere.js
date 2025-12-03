f2db545fbd39e4a402733075ab3f94592c690618
Daniel jessen <daje24ae@student.cbs.dk>
2025-12-03 12:23:47 +0100

    Replace database class with user data array

--- disprojekt2025/db/brugere.js
+++ disprojekt2025/db/brugere.js
@@ -1,30 +1,14 @@
-// db/database.js
-const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
-
-class Database {
-  constructor() {
-    this.dbPath = path.join(__dirname, 'mindb.sqlite');
-    this.db = null;
-  }
-
-  connect() {
-    this.db = new sqlite3.Database(this.dbPath, (err) => {
-      if (err) {
-        console.error('Error opening database:', err);
-      } else {
-        console.log('Connected to SQLite database');
-      }
-    });
-    return this.db;
-  }
-
-  getDB() {
-    if (!this.db) {
-      return this.connect();
+const users = [
+    {
+        username: "fang2301",
+        password: "12345",
+        email: "alzifa633@gmail.com"
+    },
+    {
+        username: "caro234",
+        password: "p54321",
+        email: "ziyundk@hotmail.com"
     }
-    return this.db;
-  }
-}
+]
 
-module.exports = new Database();
+module.exports = users;
