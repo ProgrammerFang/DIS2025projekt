b5ef9d86d89ab35bc6af0274653b6cef697d3e94
Daniel jessen <daje24ae@student.cbs.dk>
2025-12-03 12:26:20 +0100

    gammel version
    Refactor user routes to use in-memory user data and async/await.

--- disprojekt2025/routes/users.js
+++ disprojekt2025/routes/users.js
@@ -1,107 +1,51 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-const sqlite3 = require('sqlite3').verbose();
+var express = require('express');
+var router = express.Router();
+var users = require('../db/brugere');
 
-const dbPath = path.join(__dirname, '..', 'db', 'mindb.sqlite');
-const db = new sqlite3.Database(dbPath);
-
-/* GET alle brugere */
-router.get('/', (req, res, next) => {
-  db.all("SELECT id, username, email, created_at FROM users", [], (err, rows) => {
-    if (err) {
-      return next(err);
-    }
-    if (!rows || rows.length === 0) {
-      return res.status(404).json({ message: 'Ingen brugere fundet' });
-    }
-    res.json(rows);
-  });
-});
-
-/* GET bruger ud fra brugernavn */
-router.get('/:username', (req, res, next) => {
+/* GET brugere ud fra brugernavn */
+router.get('/:username', async (req, res) => {
   const username = req.params.username;
+  const user = users.find((u) => u.username === username);
   
-  db.get(
-    "SELECT id, username, email, created_at FROM users WHERE username = ?",
-    [username],
-    (err, row) => {
-      if (err) {
-        return next(err);
-      }
-      if (!row) {
-        return res.status(404).json({ message: 'Bruger ikke fundet!' });
-      }
-      res.json(row);
-    }
-  );
+  if (!user) {
+    return res.status(404).json({ message: 'User not found!' });
+  }
+
+  res.json(user);
 });
 
-/* GET bruger ud fra ID */
-router.get('/id/:id', (req, res, next) => {
-  const id = req.params.id;
-  
-  db.get(
-    "SELECT id, username, email, created_at FROM users WHERE id = ?",
-    [id],
-    (err, row) => {
-      if (err) {
-        return next(err);
-      }
-      if (!row) {
-        return res.status(404).json({ message: 'Bruger ikke fundet!' });
-      }
-      res.json(row);
-    }
-  );
+/* POST lav en ny bruger */
+router.post('/create', async (req, res) => {
+  const newUser = req.body;
+  users.push(newUser);
+  res.status(201).json(newUser);
 });
 
 /* PUT opdater bruger ud fra brugernavn */
-router.put('/update/:username', (req, res, next) => {
-  const username = req.params.username;
-  const { email, password } = req.body;
+router.put('/update/:username', async (req, res) => {
+    const username = req.params.username;
+    const updatedData = req.body;
+    const userIndex = users.findIndex((u) => u.username === username);
 
-  db.run(
-    "UPDATE users SET email = ?, password = ? WHERE username = ?",
-    [email, password, username],
-    function(err) {
-      if (err) {
-        return next(err);
-      }
-      if (this.changes === 0) {
-        return res.status(404).json({ message: 'Bruger ikke fundet!' });
-      }
-      
-      res.json({ 
-        message: 'Bruger opdateret!',
-        changes: this.changes 
-      });
+    if (userIndex === -1) {
+        return res.status(404).json({ message: 'User not found!' });
     }
-  );
+
+    users[userIndex] = { ...users[userIndex], ...updatedData };
+    res.json(users[userIndex]);
 });
 
 /* DELETE slet bruger ud fra brugernavn */
-router.delete('/delete/:username', (req, res, next) => {
-  const username = req.params.username;
+router.delete('/delete/:username', async (req, res) => {
+    const username = req.params.username;
+    const userIndex = users.findIndex((u) => u.username === username);
 
-  db.run(
-    "DELETE FROM users WHERE username = ?",
-    [username],
-    function(err) {
-      if (err) {
-        return next(err);
-      }
-      if (this.changes === 0) {
-        return res.status(404).json({ message: 'Bruger ikke fundet!' });
-      }
-      
-      res.json({ 
-        message: 'Bruger slettet!',
-        changes: this.changes 
-      });
+    if (userIndex === -1) {
+        return res.status(404).json({ message: 'User not found!' });
     }
-  );
+
+    users.splice(userIndex, 1);
+    res.json({ message: 'User deleted!' });
 });
 
 module.exports = router;
