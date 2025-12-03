7a42413b1fe58ff4fa8d999fe3097a1a61618a8a
ProgrammerFang <alfa24ae@student.cbs.dk>
2025-12-03 13:50:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use(session({
   cookie: {
     httpOnly: true,
     sameSite: 'lax',
-    secure: 'false', // ændret til false
+    secure: 'true',
     maxAge: 30 * 60 * 1000 // 30 minutter
   }
 }));
